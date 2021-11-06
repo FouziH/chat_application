8d95a0620aa19e36c53dca4fc65fe79754d5929a
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-06 18:51:40 -0500

    added chatfeed components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine, ChatFeed } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed'
 
 const App  = () => {
     return(
         <ChatEngine
             height= "100vh"
-            projectID =""
-            userName=""
-            userSecret=""
-        
+            projectID = "b17b4621-da49-4604-965c-90679ed82192"
+            userName="HamzaFowzi1"
+            userSecret="Moha0901#"
+            renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
     )
 }
