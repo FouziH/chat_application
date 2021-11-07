7daaba769c2e1061cd7389bd0872f7cc21bd12ce
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-07 06:35:05 -0600

    added or fixed small issue

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,77 +3,63 @@ import TheirMessage from "./TheirMessage";
 import MessageForm from "./MessageForm";
 
 const ChatFeed = (props) => {
-  const { chats, activeChat, userName, messages } = props;
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState("");
 
-  const chat = chats && chats[activeChat];
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-  const renderReadReceipts = (message, isMyMessage) =>
-    chat.people.map(
-      (person, index) =>
-        person.last_read === message.id && (
-          <div
-            key={`read_${index}`}
-            className="read-receipt"
-            style={{
-              float: isMyMessage ? "right" : "left",
-              backgroundImage:
-                person.person.avatar && `url(${person.person.avatar})`,
-            }}
-          />
-        )
-    );
-
-  const renderMessages = () => {
-    const keys = Object.keys(messages);
-
-    return keys.map((key, index) => {
-      const message = messages[key];
-      const lastMessageKey = index === 0 ? null : keys[index - 1];
-      const isMyMessage = userName === message.sender.username;
-
-      return (
-        <div key={`msg_${index}`} style={{ width: "100%" }}>
-          <div className="message-block">
-            {isMyMessage ? (
-              <MyMessage message={message} />
-            ) : (
-              <TheirMessage
-                message={message}
-                lastMessage={messages[lastMessageKey]}
-              />
-            )}
-          </div>
-          <div
-            className="read-receipts"
-            style={{
-              marginRight: isMyMessage ? "18px" : "0px",
-              marginLeft: isMyMessage ? "0px" : "68px",
-            }}
-          >
-            {renderReadReceipts(message, isMyMessage)}
-          </div>
-        </div>
-      );
-    });
+  const authObject = {
+    "Project-ID": projectID,
+    "User-Name": username,
+    "User-Secret": password,
   };
 
-  if (!chat) return <div />;
+  try {
+    await axios.get("https://api.chatengine.io/chats", { headers: authObject });
 
-  return (
-    <div className="chat-feed">
-      <div className="chat-title-container">
-        <div className="chat-title">{chat?.title}</div>
-        <div className="chat-subtitle">
-          {chat.people.map((person) => ` ${person.person.username}`)}
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    window.location.reload();
+    setError("");
+  } catch (err) {
+    setError("Oops, incorrect credentials.");
+  }
+};
+
+return (
+  <div className="wrapper">
+    <div className="form">
+      <h1 className="title">Chat Application</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="input"
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          placeholder="Password"
+          required
+        />
+        <div align="center">
+          <button type="submit" className="button">
+            <span>Start chatting</span>
+          </button>
         </div>
-      </div>
-      {renderMessages()}
-      <div style={{ height: "100px" }} />
-      <div className="message-form-container">
-        <MessageForm {...props} chatId={activeChat} />
-      </div>
+      </form>
+      <h1>{error}</h1>
     </div>
-  );
+  </div>
+);
 };
 
 export default ChatFeed;
