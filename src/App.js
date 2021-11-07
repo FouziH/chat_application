ea2b22aa4c9b09ef0fbf6180251ce4212e264155
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-06 22:24:49 -0500

    deleted old information and created a new one

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import LoginForm from './components/LoginForm'
 import ChatFeed from './components/ChatFeed';
+const projectID = '6e620256-73f3-4093-a9c6-3a8810e72929'
 const App  = () => {
 
     if(!localStorage.getItem('username'))  return <LoginForm />
     return(
         <ChatEngine
             height= "100vh"
-            projectID = "b17b4621-da49-4604-965c-90679ed82192"
+            projectID = {projectID}
             userName= {localStorage.getItem("username")}
             userSecret={localStorage.getItem("password")}
             renderChatFeed ={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
