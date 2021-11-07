8c7996863c32a9f6cdc0cd213c869f046eaa39b1
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-06 20:42:49 -0500

    building the logic and message form for messsage form component

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,12 +1,20 @@
  import React, {useState} from "react"
- const MessageForm = () => {
-     const [message,setMessage ] = useState('')
+ import { sendMessage, isTyping } from "react-chat-engine"
+ const MessageForm = (props) => {
+     const [message,setMessage ] = useState('');
+     const {chatId, creds} = props;
 
-    const handleChange =() => {
+    const handleChange =(event) => {
+        setMessage(event.target.value)
+        isTyping(props, chatId)
 
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+        if(text.length > 0 ) sendMessage(creds, chatId, { text })
 
     }
 
