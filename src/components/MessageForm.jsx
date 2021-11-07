f9ac101699ff91d6ee0e0fa681c2ef79dbea2ce6
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-11-06 20:52:18 -0500

    added icon into the label

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
  import React, {useState} from "react"
  import { sendMessage, isTyping } from "react-chat-engine"
+ import { SendOutlined, PictureOutlined } from "@ant-design/icons";
  const MessageForm = (props) => {
      const [message,setMessage ] = useState('');
      const {chatId, creds} = props;
@@ -23,6 +24,12 @@
     return(
        <form className="message-form" onSubmit={handleSubmit}>
            <input className="message-input" placeholder="Send a message..." value={message}  onChange={handleChange} onSubmit={handleSubmit}/>
+           <label htmlFor="upload-button">
+               <span className="image-button">
+                   {<PictureOutlined  className="picture-icon"/>}
+               </span>
+
+           </label>
            
        </form>
     )
